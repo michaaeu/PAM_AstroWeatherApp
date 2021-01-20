39f4509da4592966052689ee90a02c3c67b8d443
Michał <mik3wwl@gmail.com>
2021-01-20 14:19:17 +0100

    Added WeatherAPI constructor with APIkey

--- app/src/main/java/com/example/astroweatherapp/AstroWeather/AstroWeatherInfo.java
+++ app/src/main/java/com/example/astroweatherapp/AstroWeather/AstroWeatherInfo.java
@@ -1,7 +1,20 @@
 package com.example.astroweatherapp.AstroWeather;
 
-import com.astrocalculator.AstroCalculator;
-
 public class AstroWeatherInfo {
+    private final String URL;
+    private final String key;
+    private String city;
 
+    public AstroWeatherInfo(){
+        this.URL = "api.openweathermap.org/data/2.5/weather?q=";
+        this.key = "34986c4e28ef11c3a2cc09861086536b";
+        this.city = "Warsaw";
+    }
+    public AstroWeatherInfo(String city){
+        this();
+        this.city = city;
+    }
+    public void test(){
+        System.out.println("URL: " + this.URL + "\nKey: " + this.key + "\nCity: " + this.city);
+    }
 }
