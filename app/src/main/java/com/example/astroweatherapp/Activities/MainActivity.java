d1c9f56f66bfadc22b5dc19504181c810943da50
Michał <mik3wwl@gmail.com>
2021-01-20 14:19:40 +0100

    Imersive navigation fixed

--- app/src/main/java/com/example/astroweatherapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/astroweatherapp/Activities/MainActivity.java
@@ -8,36 +8,38 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.astroweatherapp.Adapter.FragmentAdapter;
+import com.example.astroweatherapp.AstroWeather.AstroWeatherInfo;
 import com.example.astroweatherapp.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    AstroWeatherInfo astroWeatherInfo;
     ViewPager pager;
     FragmentAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        astroWeatherInfo = new AstroWeatherInfo();
 
         pager = findViewById(R.id.pager);
         adapter = new FragmentAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
+        setWindow();
+        astroWeatherInfo.test();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+    public void setWindow() {
         //This is used to hide/show 'Status Bar' & 'System Bar'. Swip bar to get it as visible.
         View decorView = getWindow().getDecorView();
-        if (hasFocus) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     public void settingsButton(View view) {
--- app/src/main/java/com/example/astroweatherapp/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/astroweatherapp/Activities/SettingsActivity.java
@@ -23,22 +23,18 @@ public class SettingsActivity extends AppCompatActivity {
         stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         refreshPeriodSpinner.setAdapter(stringArrayAdapter);
-
+        setWindow();
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+    public void setWindow() {
         //This is used to hide/show 'Status Bar' & 'System Bar'. Swip bar to get it as visible.
         View decorView = getWindow().getDecorView();
-        if (hasFocus) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
